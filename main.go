4c31d1281f31a1710caf64e42601aabec0de00a3
zayn1510 <zayndevs.1510@gmail>
2025-04-06 22:03:43 +0800

    feat : add readme documentation

--- main.go
+++ main.go
@@ -1,21 +1,14 @@
 package main
 
 import (
-	"apidanadesa/app/controllers"
-	"apidanadesa/app/middleware"
-	"apidanadesa/routers"
 	"github.com/gin-gonic/gin"
+	"github.com/zayn1510/goarchi/app/middleware"
+	"github.com/zayn1510/goarchi/routers"
 )
 
-func SetUpRouterBidang(rg *gin.RouterGroup) {
-	c := controllers.NewControllerBidang()
-	bidang := rg.Group("/bidang")
-	bidang.GET("/", c.GetAllBidangs)
-}
 func main() {
 	router := gin.Default()
 	middleware.SetCors(router)
-	router.Use(middleware.ErrorHandler())
 	routers.RegisterRoutes(router)
 	router.Run(":8080")
 }
